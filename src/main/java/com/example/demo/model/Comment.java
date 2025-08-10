c1d081caad5939d2fde2a11d7fd5e6519b781e86
mz7701 <korean2071@naver.com>
2025-08-10 13:09:08 +0900

    Revert "v3"
    This reverts commit 99826d36

--- src/main/java/com/example/demo/model/Comment.java
+++ src/main/java/com/example/demo/model/Comment.java
@@ -1,6 +1,13 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 기본키
+
     private String name;
     private String content;
 
@@ -11,10 +18,13 @@ public class Comment {
         this.content = content;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -22,7 +32,6 @@ public class Comment {
     public String getContent() {
         return content;
     }
-
     public void setContent(String content) {
         this.content = content;
     }
